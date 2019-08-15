29ad66dafcc45546cbbff9c93e4282c9a86e3b56
Pereyra <pereyra3@whiteoak.llnl.gov>
2019-08-14 17:10:56 -0700

    gmsh two body spherical mesh

--- incomp_navierstokes/sphereinbox2body/createJobs.py
+++ incomp_navierstokes/sphereinbox2body/createJobs.py
@@ -1,31 +1,55 @@
-import os,sys,params
+import os,sys,params, argparse
 
+#input arguments
+parser= argparse.ArgumentParser(prog='simResults.py', description='Process simulation CL,CD,CSF results.')
+parser.add_argument('--dir',dest='dirpath',action='store',help='file directory to use (relative to working directory)')
+parser.add_argument('-d',dest='diameter',action='store',help='sphere diameter')
+parser.add_argument('-l',dest='far',action='store',help='sphere far-field length')
+parser.add_argument('-s',dest='separation',action='store',help='sphere separation')
+parser.add_argument('-a',dest='angle',action='store',help='sphere angle')
+args= parser.parse_args()
+
+if not args.dirpath:
+        print("Error: provide --dir <create mesh>")
+        exit()
+
+dirpath= args.dirpath
+
+#from params
 dia=params.min_dia
 far=params.min_far
 
-if params.inc_sep is not 0: 
-	ndists= int((params.max_sep-params.min_sep)/params.inc_sep)+1
+if params.inc_far is not 0: 
+	nfar= int((params.max_far-params.min_far)/params.inc_far)+1
 else:
-	ndists= 1
+	nfar= 1
 
-if params.inc_angle is not 0: 
-	nangles= int((params.max_angle-params.min_angle)/params.inc_angle)+1
+if params.inc_node is not 0: 
+	nnode= int((params.max_node-params.min_node)/params.inc_node)+1
 else:
-	nangles= 1
+	nnode= 1
 
 print("ndists={} nangles={}".format(ndists,nangles))
 
 with open("job","w") as jobsfile:
 	jobsfile.write("g++ -std=c++11 -Iinclude sphere2body.cpp -o run -lgmsh\n")
 
-for dist_idx in range(0,ndists):
-	dist= params.min_sep+dist_idx*params.inc_sep
+for far_idx in range(0,nfar):
+	far= params.min_far+far_idx*params.inc_far
 
-	for ang_idx in range(0,nangles):
-		ang= params.min_angle+ang_idx*params.inc_angle
+	for node_idx in range(0,nnode):
+		node= params.min_node+node_idx*params.inc_node
+
 		with open("job","a") as jobsfile:
-			jobsfile.write("python createDirPlusSU2.py {} {} {} {}\n".format(dia,far,dist,ang))
-			jobsfile.write("cd data/dia_{}_len_{}_sep_{}_ang_{}\n".format(dia,far,dist,ang))
-			#jobsfile.write("../.././run {} {} {} {}\n".format(dia,far,dist,ang))
-			#jobsfile.write("SU2_CFD lam_sphere.cfg\n")
+			jobsfile.write("python createDirSlurm.py {} {} {}\n".format(dia,far,node))
+			jobsfile.write("cd meshes/dia_{}_far_{}_nodes_{}\n".format(dia,far,node))
+			jobsfile.write("../.././run {} {} {} {}\n".format(dia,far,dist,ang))
+			jobsfile.write("cp ../../dataAnalysisScript.py .\n")
+			jobsfile.write("cp ../../lam_sphere_inc.cfg .\n")
 			jobsfile.write("cd ../..\n\n")
+
+with open("job","a") as jobsfile:
+        jobsfile.write("cd {}/\n".format(dirpath))
+        jobsfile.write("cp ../params.py .\n")
+        jobsfile.write("cd ../\n")
+        jobsfile.close()
--- incomp_navierstokes/sphereinbox2body/params.py
+++ incomp_navierstokes/sphereinbox2body/params.py
@@ -1,12 +1,19 @@
 min_dia=1
 max_dia=1
 inc_dia=0
-min_far=4
-max_far=4
+
+min_far=7
+max_far=7
 inc_far=0
-min_sep=1.2
-max_sep=1.2
+
+min_sep=4
+max_sep=4
 inc_sep=0
+
 min_angle= 0
 max_angle= 0
 inc_angle= 0
+
+min_node= 19
+max_node= 51
+inc_node= 2
